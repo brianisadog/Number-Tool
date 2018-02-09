3aad16b7edf9ceb31323a4e4399cb393840b1bd8
BrianSungUSFCA <csung4@dons.usfca.edu>
2018-02-09 21:16:36 +0000

    Finished convert to binary

--- nt.c
+++ nt.c
@@ -7,7 +7,10 @@ bool parse_argv(int, char **, char *, int *, int *, int *);
 void set_start_end(char *, int *, int *);
 void convert_to_binary(char *, char *);
 void binary_to_binary(char *, int, char *);
-void deciaml_to_binary(char *, int);
+void hexadecimal_to_binary(char *, int, char *);
+void string_to_binary(char *, int, char *);
+void unsigned_decimal_to_binary(long, char *);
+void to_negative(char *);
 void print_binary_4_chunk(char *);
 void print_binary(char *);
 
@@ -77,6 +80,12 @@ void convert_to_binary(char *arg, char *bits) {
     if (len > 2 && arg[1] == 'b') {
         binary_to_binary(arg, len, bits);
     }
+    else if (len > 2 && arg[1] == 'x') {
+        hexadecimal_to_binary(arg, len, bits);
+    }
+    else {
+        string_to_binary(arg, len, bits);
+    }
 
     bits[32] = '\0';
 }
@@ -89,12 +98,75 @@ void binary_to_binary(char *arg, int len, char *bits) {
     }
 }
 
-void decimal_to_binary(char *bits, int input) {
-    int i = 31;
-    while (i >= 0) {
-        bits[i] = '0' + input % 2;
-        input /= 2;
-        i--;
+void hexadecimal_to_binary(char *arg, int len, char *bits) {
+    int i;
+    char c;
+    long unsigned_decimal;
+
+    for (i = 2, unsigned_decimal = 0; i < len; i++) {
+        c = arg[i];
+        unsigned_decimal *= 16;
+        unsigned_decimal += (c >= 'A' && c <= 'F') ? (10 + c - 'A') : (0 + c - '0');
+    }
+    
+    unsigned_decimal_to_binary(unsigned_decimal, bits);
+}
+
+void string_to_binary(char *arg, int len, char *bits) {
+    int i;
+    unsigned int temp;
+    char c;
+    long unsigned_decimal;
+    bool negative = false;
+
+    if (arg[0] == '-') {
+        negative = true;
+        i = 1;
+    }
+    else {
+        i = 0;
+    }
+    
+    for (unsigned_decimal = 0; i < len; i++) {
+        c = arg[i];
+        unsigned_decimal = unsigned_decimal * 10 + (0 + c - '0');
+    }
+
+    unsigned_decimal_to_binary(unsigned_decimal, bits);  
+
+    if (negative) {
+        to_negative(bits);
+    }
+}
+
+void unsigned_decimal_to_binary(long unsigned_decimal, char *bits) {
+    int i;
+    
+    for (i = 31; i >= 0; i--) {
+        bits[i] = (unsigned_decimal & 0b1) == 0 ? '0' : '1';
+        unsigned_decimal >>= 1;
+    }
+}
+
+void to_negative(char *bits) {
+    int i;
+    char carry = '1';
+
+    for (i = 31; i >= 0; i--) {
+        bits[i] = bits[i] == '0' ? '1' : '0';
+    }
+
+    for (i = 31; i >= 0; i--) {
+        if (carry == '1' && bits[i] == '1') {
+            bits[i] = '0';
+        }
+        else if (carry == '1' && bits[i] == '0') {
+            bits[i] = '1';
+            carry = '0';
+        }
+        else {
+            break;
+        }
     }
 }
 
